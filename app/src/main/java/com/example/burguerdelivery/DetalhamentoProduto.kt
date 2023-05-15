ff07c6509d29cf146b061250287aa512e5b49b7c
João Vichtor Loquingen <106892933+joaoloquingen@users.noreply.github.com>
2023-05-14 23:19:33 -0300

    Button interation

--- app/src/main/java/com/example/burguerdelivery/DetalhamentoProduto.kt
+++ app/src/main/java/com/example/burguerdelivery/DetalhamentoProduto.kt
@@ -1,15 +1,23 @@
 package com.example.burguerdelivery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.burguerdelivery.databinding.ActivityDetalhamentoProdutoBinding
-import com.example.burguerdelivery.databinding.ActivityMainBinding
 
-class DetalhamentoProdutoActivity : AppCompatActivity() {
+class DetalhamentoProduto : AppCompatActivity() {
+
+
     private lateinit var binding: ActivityDetalhamentoProdutoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetalhamentoProdutoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnVoltar.setOnClickListener {
+            val voltarTela = Intent(this, MainActivity::class.java )
+            startActivity(voltarTela)
+        }
     }
 }
